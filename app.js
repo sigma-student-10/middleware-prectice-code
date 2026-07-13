2bbfccdeaef90c0407c4f102b234127c59ec140c
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-07-13 13:31:03 +0600

    add new file

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+
+// app.use( (req, res,next) => {
+//     console.log("Hi , I am middleware");
+//     next();
+// });
+
+// app.use( (req, res,next) => {
+//     console.log("Hi , I am 2nd middleware");
+//     next();
+// });
+
+// app.use("/", (req, res, next) => {
+//     console.log("I am only for random");
+//     next();
+// });
+
+const checkToken =  (req, res, next) => {
+    let { token } = req.query;
+    if(token === "giveaccess") {
+        next()
+    }
+    throw new Error("ACCESS DENIED!");
+};
+
+// app.get("/wrong", (req, res) => {
+//     abcd = abcd;
+// })
+
+app.get("/api", checkToken, (req, res) => {
+    res.send("data");
+});
+
+app.get("/", (req, res) => {
+    res.send( "Hi, I am root. ");
+});
+
+app.get("/random", (req, res) => {
+    res.send("this is a random page");
+});
+
+// //logger
+// app.use((req, res, next) => {
+//     req.time = Date.now();
+//     console.log(req.method, req.hostname, req.path, req.time );
+//     next();
+// });
+
+//404
+app.use((req, res) => {
+    res.send("Page not found");
+});
+
+
+app.listen(8080, () => {
+    console.log("server listening to port 8080");
+});
